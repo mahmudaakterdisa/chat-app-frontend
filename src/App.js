aae1c6ef5d19861332f86d03b2f395edf4f7994e
mahmudaakterdisa <dishamirza.h@gmail.com>
2023-06-29 20:19:21 +0200

    frontend sub-module created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  console.log(messages);
+  useEffect(() => {
+    getMessages();
+    setInterval(getMessages, 5000);  // Polling every 5 seconds
+  }, []);
+
+  const getMessages = async () => {
+    const response = await axios.get('http://localhost:5000/messages');
+    setMessages(response.data.results);
+  };
+
+  const sendMessage = async () => {
+    await axios.post('http://localhost:5000/messages', { username, message });
+    setMessage('');
+    getMessages();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
+      <input type="text" value={message} onChange={e => setMessage(e.target.value)} placeholder="Enter message" />
+      <button onClick={sendMessage}>Send</button>
+      {messages && messages.map(msg => <div key={msg.id}>{msg.username}: {msg.message}</div>)}
     </div>
   );
 }
